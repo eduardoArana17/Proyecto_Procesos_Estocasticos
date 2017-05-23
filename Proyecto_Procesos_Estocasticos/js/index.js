9634045c25c1dd2007eabb09d3ab9694f51a5e11
eduardoArana17 <eduardo_arana15@hotmail.com>
2017-05-23 02:45:09 -0500

    index.js

--- Proyecto_Procesos_Estocasticos/js/index.js
+++ Proyecto_Procesos_Estocasticos/js/index.js
@@ -1,75 +1,63 @@
 $(document).ready(function() {
     console.log("document ready");    
 
-    $("#cmbMatrixSize").on("change", function(){
-        $("#cmbMatrixSize").attr("disabled", true);        
-        $("#divData").show();
-        createMatrix($("#cmbMatrixSize").val());
-        createPolicy($("#cmbMatrixSize").val());
-		showStates($("#cmbMatrixSize").val());
-    });
-
     $("#btnState").on("click", function(){        
-        showStates($("#stateNumber").val());        
+        var sizeState = $("#stateNumber").val();
+        var sizeDecision = $("#decisionNumber").val();
+
+        if(Number(sizeState) <= 1 || Number(sizeDecision) <= 0){
+            $.msgBox({
+                title:"Error.",
+                content:"Deben existir por lo menos 2 estados y 1 decisión."
+            });
+            return false;
+        }
+
+        $("#divData").show();
+        showCosts(sizeState, sizeDecision);   
+        createMatrix(sizeState);        
     });
 });
 
+function showCosts(sizeState, sizeDecision){    
+    $("#costs").show(); 
+    $("#costs").html(""); 
+    var stateHtml = ""; 
+    for (var i = 0; i < sizeState; i++) {
+        for(var j = 0; j < sizeDecision; j++){
+            stateHtml += "<label>Costo" + (i + 1) + (j + 1) + ":</label><input type='number'" +
+                    "class='form-control' id='cost" + (i + 1) + (j + 1) + "' min='0' />";
+        }        
+    }
+    $("#costs").html(stateHtml);  
+}
+
 function createMatrix(size){ 
+    console.log(size);    
     $("#matrix").html("");   
     var tableHtml = ""; 
     for (var i = 0; i < size; i++) { 
         tableHtml += "<tr>";                   
         for (var j = 0; j < size; j++) {                    
-            tableHtml += "<td><input type='number' id='" + i + "matrix" + j + "' name='" + i + "matrix" + j + "' value=''/> </td>";
+            tableHtml += "<td><input type='number' id='matrix" + i +  j + "'' value=''/> </td>";
         }
         tableHtml += "</tr>";   
     }
     $("#matrix").html(tableHtml);
 }     
 
-function createPolicy(size){
-    $("#policy").html(""); 
-    var tableHtml = ""; 
-    tableHtml += "<tr>";
-    for (var i = 0; i < size; i++) {                             
-        tableHtml += "<td><input type='number' id='policy" + i + "' name='policy" + i + "' value=''/> </td>";                
-    }
-    tableHtml += "</tr>";   
-    $("#policy").html(tableHtml);  
-}
-
-function showStates(size){
-    if(Number(size) <= 0){
-        $.msgBox({
-            title:"Error.",
-            content:"Favor de ingresar un valor mayor a 0."
-        });
-        return false;
-    }
-
-    $("#states").show(); 
-    $("#states").html(""); 
-    var stateHtml = ""; 
-    for (var i = 0; i < size; i++) {
-        stateHtml += "<label>Costo del Estado " + (i + 1) + ":</label><input type='number'" +
-                    "class='form-control' id='stateNumber" + (i + 1) + "' name='stateNumber" + (i + 1) + "' min='0' />";
-    }
-    $("#states").html(stateHtml);  
-}
-
 function clean(){
-    $("#divData").hide();
-    $("#cmbMatrixSize").val("");  
-    $("#cmbMatrixSize").attr("disabled", false);  
-    $("#states").html(""); 
+    $("#divData").hide();    
+    $("#costs").html("");
+    $("#matrix").html(""); 
     $("#stateNumber").val(""); 
+    $("#decisionNumber").val(""); 
 }
 
 function save(){
     var error = 0;
     $('#formData :input:not(:button)').each(function ()
-    {
-        console.log(this);
+    {        
         if ($.trim(this.value) == "")
             error++;
     });
@@ -80,47 +68,26 @@ function save(){
         });
     }
     else{
-        var formObj = $('#formData').serializeObject();
-        formObj.size = $("#cmbMatrixSize").val();
-		
-		var dim =  formObj.size * formObj.size;
-		
-		formObj.MatrixArray = [];
-		var count = 0;
-		for(i = 0; i < formObj.size; i++){
-			
-			for(j = 0; j < formObj.size; j++){
+        var formObj = $('#formData').serializeObject();        
+        formObj.size = formObj.stateNumber;
+		console.log(formObj);		
+		        
+		formObj.MatrixArray = [];	        
+		for(var i = 0; i < formObj.size; i++){			
+			for(var j = 0; j < formObj.size; j++){								
+				formObj.MatrixArray.push($("#matrix"+ i + j).val());							                
+			}			
+		}						
+		        
+		formObj.CostArray = [];		
+		for(var i = 0; i < formObj.stateNumber; i++){            
+            for(var j = 0; j < formObj.decisionNumber; j++){    
+                console.log("#cost" + (i + 1) + (j + 1));
+                console.log($("#cost"+ (i + 1) + (j + 1)).val());                          
+                formObj.CostArray.push($("#cost"+ (i + 1) + (j + 1)).val());                                  
+            }           
+        }   
 				
-				console.log("#"+i+"matrix"+j);
-				console.log($("#"+i+"matrix"+j).val());
-				formObj.MatrixArray[count] = $("#"+i+"matrix"+j).val();
-				count++;
-				
-			}
-			// count++;
-		}		
-		
-		count = 0;
-		formObj.PolicyArray = [];		
-		for(k = 0; k < formObj.size; k++){
-				
-				console.log("#policy"+k);
-				formObj.PolicyArray[count] = $("#policy"+k).val();
-				count++;
-				
-		}
-		
-		count = 0;
-		formObj.StateArray = [];		
-		for(l = 0; l < formObj.size; l++){
-				
-				console.log("#stateNumber"+(count+1));
-				formObj.StateArray[count] = $("#stateNumber"+(count+1)).val();
-				count++;
-				
-		}
-		
-		
         localStorage['formObj'] = JSON.stringify(formObj);
 
         $.msgBox({
@@ -128,9 +95,6 @@ function save(){
             content:"La información se ha guardado correctamente.",
             type:"info"
         });
-        console.log(JSON.parse(localStorage['formObj']));
-		
-		JSON.parse(localStorage['formObj']);
-		
+        console.log(JSON.parse(localStorage['formObj']));				
     }    
 }
